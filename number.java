fe731ffdf6866eba7a8d0293a0346ba9c959d8dd
suryajai <arasidpharm@gmail.com>
2017-07-20 09:28:20 +0530

    Add files via upload

---
+++ number.java
@@ -0,0 +1,23 @@
+
+public class number {
+	public static void main(String[] args) {
+
+        int num = 30;
+        boolean flag = false;
+        for(int i = 4; i <= num/2; ++i)
+        {
+            if(num % i == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
+    }
+}
+
+
